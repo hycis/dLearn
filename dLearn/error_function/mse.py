225a77f09f01077400ce3512310e77f2d4c83399
hycis <hyciswu@gmail.com>
2013-08-26 15:07:06 -0400

    change mlp_layer to layer

--- dLearn/error_function/mse.py
+++ dLearn/error_function/mse.py
@@ -4,7 +4,7 @@ Created on Aug 26, 2013
 @author: zhenzhou
 '''
 
-class MSE():
+class MSE(object):
     
     def __init__(self):
         pass
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pylearn2.datasets.mnist import MNIST
 
 from dLearn.learning_model.mlp_layer.softmax import SoftMax
 from dLearn.learning_model.mlp import MLP
-from dlearn.error_function.mse import MSE
+from dLearn.error_function.mse import MSE
 
 def main():
     
@@ -12,11 +12,11 @@ def main():
     h3 = SoftMax()
     y_layer = SoftMax()
                 
-    mlp = MLP(input_space,
-              layers=[h1, h2, h3, y_layer],
+    mlp = MLP(layers=[h1, h2, h3, y_layer],
               error_function = MSE(),
               dataset = MNIST(),
-              batch_size)
+              batch_size = 100
+              )
     
     mlp.setup()
     mlp.train()
