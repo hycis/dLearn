193c9ba9844ad683df982ff51543b7226c0dc7eb
hycis <hyciswu@gmail.com>
2013-09-02 23:44:37 -0400

    added new ignores to gitignore

---
+++ test/test.py
@@ -0,0 +1,16 @@
+import numpy
+import theano
+import theano.tensor as T
+rng = numpy.random
+
+
+class test:
+    
+    def __init__(self, x=1):
+        print x
+        print self.x
+        
+
+
+if __name__ == '__main__':
+    f = test()
