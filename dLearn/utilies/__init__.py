c9db4a1ededcb3611f19d09843ef1533e9d53fcf
Zhenzhou Wu <hyciswu@gmail.com>
2013-09-11 21:58:55 -0400

    update

--- dLearn/utilies/__init__.py
+++ dLearn/utilies/__init__.py
@@ -4,26 +4,47 @@ matplotlib.use('Agg')
 import cPickle
 import matplotlib.pyplot as plt
 
-def plot_error(batches, errors, legends, save_path='plot.png'):
+def plot_error(batch_error_list, legends, save_path='plot.png', axis_limits=[0, 25000, 0, 0.15]):
     '''
-    batches: list of batch pkl files
-    errors: list of error pkl files
-    legends: list of legends pkl files
+    error_list: list of pkl files, where each pkl file contains [batch_num, test_error]
+    legends: list of legends for the datasets
     '''
     
-    assert len(batches) == len(errors) == len(legends)
+    assert len(batch_error_list) == len(legends)
     
     plots = []
-    legs =[]
-    for i in xrange(len(batches)):
-        bat = cPickle.load(open(batches[i], 'rb'))
-        err = cPickle.load(open(errors[i], 'rb'))
-        leg = cPickle.load(open(legends[i], 'rb'))
-        legs += leg
-        plots.append(plt.plot(bat, err))
+    for i in xrange(len(batch_error_list)):
+        bat_err_ls = cPickle.load(open(batch_error_list[i], 'rb'))
+        plots.append(plt.plot(bat_err_ls[0], bat_err_ls[1]))
         
-    
-    plt.legend(plots, legs)
+    plt.axis(axis_limits)
+    plt.legend(plots, legends)
     plt.savefig(save_path)
+
+
+    
+def test_learning_rates(model, learning_rates=[0.001, 0.005, 0.01, 0.05, 0.1, 0.5], 
+                        num_batches=10000, plot=False, axis_limits=[0, 25000, 0, 0.15],
+                        save_path='learning_rate.png'):
+    
+    plots = []
+    legends = []
+    for learning_rate in learning_rates:
+        model.set_learning_rate(learning_rate)
+        model.train_batch(num_batches)
+        
+        with open('{}.pkl'.format(learning_rate)) as f:
+            list = [model.batch_num, model.test_error]
+            cPickle.dump(list, f)
+        
+        if plot:
+            plots.append(plt.plot(model.batch_num, model.test_error))
+            legends.append(learning_rate)
+    
+    if plot:
+        plt.axis(axis_limits)
+        plt.legend(plots, legends)
+        plt.savefig(save_path)
+            
  
         
